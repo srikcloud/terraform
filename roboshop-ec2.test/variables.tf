3b8da9ec1c230df66850cdb4918d28ce21683b60
srikanth <srikanth.baireddy@outlook.com>
2025-06-17 11:47:48 +0530

    terraform

---
+++ roboshop-ec2.test/variables.tf
@@ -0,0 +1,15 @@
+variable "security_group_ids" {
+    default = ["sg-077195e1f9b17c589"]
+}
+
+variable "tags" {
+    default = {
+        Name = "roboshop-cart"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "instance_type" {
+    default = "t3.small"
+}
