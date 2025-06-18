8be3420196b8a0b487347c3f209736a8a8bbf83c
srikanth <srikanth.baireddy@outlook.com>
2025-06-18 18:41:50 +0530

    shell-roboshop-2

--- vpc-module-test/main.tf
+++ vpc-module-test/main.tf
@@ -1,6 +1,11 @@
 module "vpc" {
     source = "../terraform-aws-vpc"
-    project = "roboshop"
+    /* project = "roboshop"
     Environment = "dev"
-    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
-}
+    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
+    project = var.project
+    Environment = var.environment
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+ }
