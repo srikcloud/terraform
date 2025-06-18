3887306c4447633976e653fbb613ebb0e971c019
srikanth <srikanth.baireddy@outlook.com>
2025-06-18 13:11:07 +0530

    terra commit

--- vpc-module-test/main.tf
+++ vpc-module-test/main.tf
@@ -2,6 +2,5 @@ module "vpc" {
     source = "../terraform-aws-vpc"
     project = "roboshop"
     Environment = "dev"
-    public_subnet_cidrs = ["10.0.1.0/24" , "10.0.2.0/24"]
-
+    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
 }
