83c7229e2e890d7acdeb0bda6cb561e38de35a1f
srikanth <srikanth.baireddy@outlook.com>
2025-07-02 11:41:57 +0530

    terraform

---
+++ loops/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "roboshop" {
+  count = 4
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = var.environment == "dev" ? "t3.micro" : "t3.small" 
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = { 
+    Name = var.instances[count.index]
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+    name = "allow_all"
+    description = "allow all traffic"
+   
+    ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+    Name = "allow_all"
+    }
+}
