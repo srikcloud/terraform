1f521964038ffb651060a7733d2566ba1faa4fde
srikanth <srikanth.baireddy@outlook.com>
2025-07-03 23:01:10 +0530

    terraform

---
+++ state/variables.tf
@@ -0,0 +1,50 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "AMI ID of joindevops RHEL9"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "ec2_tags" {
+    type = map(string)
+  default = {
+    Name = "Roboshop"
+    Purpose = "variables-demo"
+  }
+}
+variable "sg_name" {
+    default = "allow_all"
+
+}
+variable "sg_description" {
+  default = "allowing all ports from internet"
+}
+variable "from_port" {
+    default = 0
+}
+variable "to_port" {
+  type = number
+  default = 0
+}
+variable "cidr_blocks" {
+ type = list(string)
+ default = ["0.0.0.0/0"]
+}
+variable "sg_tags" {
+    default = {
+        Name = "allow-all"
+    }
+}
+variable "environment" {
+default = "dev"  
+}
+variable "instances" {
+ default = ["mongodb", "redis", "mysql", "rabbitmq"] 
+}
+variable "zone_id" {
+  default = "Z09795462K7LFC60CJ3VQ"
+}
+variable "domain_name" {
+  default = "srikanth553.store"
+}
